11cf583fb66bf0213a1ae985a5ed0460eb63fbbc
Simon Skoczylas <github@giftkugel.de>
2023-04-26 07:58:37 +0200

    Add seed parameter

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -10,27 +10,31 @@ public class GameRunner {
 	private static boolean notAWinner;
 
 	public static void main(String[] args) {
+		long seed = 42;
+		if (args.length == 1) {
+			seed = Long.parseLong(args[0]);
+		}
 		Game aGame = new Game();
-		
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
+
+		Random rand = new Random(seed);
+
 		do {
-			
+
 			aGame.roll(rand.nextInt(5) + 1);
-			
+
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
+
+
 		} while (notAWinner);
-		
+
 	}
 }
